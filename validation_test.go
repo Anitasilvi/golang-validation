d0ee758afa20b0405d326a50787afb8bc86385a1
Anitasilvi <anitaferdina06@gmail.com>
2023-10-02 13:32:37 +0700

    validasi basic collection

--- validation_test.go
+++ validation_test.go
@@ -175,3 +175,43 @@ func testCollection(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func testBasicCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id        string    `validate:"required"`
+		Name      string    `validate:"required"`
+		Addresses []Address `validate:"required,dive"`
+		Hobbies   []string  `validate:"required,dive,required,min=3"`
+	}
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+		Hobbies: []string{
+			"Gaming",
+			"Coding",
+			"X",
+			"",
+		},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
