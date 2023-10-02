4133b3df46f2e02c04fbfea2bb449173f53cca0c
Anitasilvi <anitaferdina06@gmail.com>
2023-10-02 20:12:45 +0700

    custom validation parameter

--- validation_test.go
+++ validation_test.go
@@ -2,6 +2,8 @@ package validation
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -388,5 +390,41 @@ func TestCustomValidation(t *testing.T) {
 	err := validate.Struct(request)
 	if err != nil {
 		fmt.Println(err.Error())
+	}
+}
+
+var regexNumber = regexp.MustCompile("^[0-9]+$")
+
+func MustValidPin(field validator.FieldLevel) bool {
+	length, err := strconv.Atoi(field.Param())
+	if err != nil {
+		panic(err)
+	}
+
+	value := field.Field().String()
+	if !regexNumber.MatchString(value) {
+		return false
+	}
+
+	return len(value) == length
+}
+
+func TestCustomValidationParameter(t *testing.T) {
+	validate := validator.New()
+	validate.RegisterValidation("pin", MustValidPin)
+
+	type Login struct {
+		Phone string `validate:"required,number"`
+		Pin   string `validate:"required,pin=6"`
+	}
+
+	request := Login{
+		Phone: "08883020723",
+		Pin:   "123456",
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
